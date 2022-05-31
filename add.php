b36745f1701610ad2f705f5794aa8b3830138bf5
wissprecicion <wiss.precicion@gmail.com>
2022-05-30 20:41:52 -0500

    ver1

--- add.php
+++ add.php
@@ -13,7 +13,7 @@ VALUES (NULL, '$categoria', $precio, $cantidad, '$observaciones', '$tamano', '$c
 $result = mysqli_query($connection,$sql);
 echo $sql;
 if ($result){
-    header('location:display');/* redirecionar */
+    header('location:display.php');/* redirecionar */
     echo "Data inserted successfully";
 }else {
     die(mysqli_error($connection));
@@ -28,16 +28,100 @@ if ($result){
     <title>Invetario | Agregar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/MisEstilos.css">
+         <link rel="icon" type="image/x-icon" href="assets/favicon-16x16.png" />
     <style>
     .mb-3 {
         margin-bottom: 2.5rem !important;
     }
+    #mainNav{
+          background-color: rgba(154, 180, 75, 0.9) !important;/* --priamry-alpha: rgba(154, 180, 75, 0.9); */
+      }
     </style>
 </head>
 
 <body>
+     <!-- nav -->
+     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand text-brand" href="index.html">
+                <img src="assets/img/logo.png" alt="" width="50px" />
+                Jardinería del Éden</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#">Inicio</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Galería</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#about1">Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#products">Prodcutos</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="#">Admin</a></li> -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                        Admin
+                    </button>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- modal -->
+    <div class="modal" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Jardinería del Éden</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <form class="form-signin">
+                        <div class="img-login mx-auto">
+                            <img class="mb-4 img-fluid " src="assets/img/logo.png" alt="" width="120">
+                        </div>
+                        <h1 class="h3 mb-3 font-weight-normal">Por favor, introduce las credenciales</h1>
+                        <!-- user -->
+                        <label for="inputEmail" class="sr-only">Usuario</label>
+                        <input type="text" id="inputEmail" class="form-control m-3 mx-auto" placeholder="Usuario"
+                            required="" autofocus="">
+                        <!-- password -->
+                        <label for="inputPassword" class="sr-only">Contraseña</label>
+                        <input type="password" id="inputPassword" class="form-control m-3 mx-auto"
+                            placeholder="Contraseña" required="">
+
+                        <!-- <button class="btn btn-lg btn-primary btn-block" type="submit" onClick="return validar()">
+                  Ir </button> -->
+                        <!-- doesnt working -->
+                        <input type="button" class="btn btn-lg btn-primary btn-block" value="Ir"
+                            onClick="return validar()">
+
+
+                        <p class="mt-5 mb-3 text-muted">© 2019-2022</p>
+                    </form>
+
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
     <div class="container my-5">
-        <p class="display-1">Llena los campos</p>
+        <p class="mt-50 display-1">Llena los campos</p>
         <form method="post">
             <!-- categoria -->
             <div class="mb-3">
@@ -115,8 +199,70 @@ if ($result){
             <button type="submit" class="btn btn-primary" name="submit">Guardar</button>
         </form>
     </div>
+
+
+    <footer class="footer_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 footer-col">
+                    <div class="footer_contact">
+                        <h4>Contactanos</h4>
+                        <div class="contact_link_box">
+                            <a href="https://goo.gl/maps/AFLsBTwAWwEGr1636" target="_blank">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                <span> Ubicación </span>
+                            </a>
+                            <a href="https://wa.me/527861015488/?text=Hola, ¿Qué tal?" target="_blank">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <span> Llama +52 786 1015488 </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <span> jardinera.eden@gmail.com </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <a href="" class="footer-logo"> Jardinería del Éden </a>
+                        <p>Vive tu vida, vive tus plantas</p>
+                        <div class="footer_social">
+                            <a href="">
+                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <h4>Horario</h4>
+                    <p>Todos los días</p>
+                    <p>10.00 Am - 10.00 Pm</p>
+                </div>
+            </div>
+            <div class="footer-info">
+                <p>
+                    &copy; <span id="displayYear"></span> Todos los derecho reservados -
+                    <i>Jardinería del Éden</i>
+                    <!-- <a href="https://html.design/">Free Html Templates</a><br><br> -->
+                    <!--   &copy; <span id="displayYear"></span> Distributed By
+              <a href="https://themewagon.com/" target="_blank">ThemeWagon</a> -->
+                </p>
+            </div>
+        </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
+     <script src="https://kit.fontawesome.com/0c7e5557d4.js" crossorigin="anonymous"></script>
 </body>
 </html>
--- display.php
+++ display.php
@@ -8,11 +8,103 @@
     <title>Inventario</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/MisEstilos.css">
+        <link rel="icon" type="image/x-icon" href="assets/favicon-16x16.png" />
+    <style>
+      #mainNav{
+          background-color: rgba(154, 180, 75, 0.9) !important;/* --priamry-alpha: rgba(154, 180, 75, 0.9); */
+      }
+    </style>
 </head>
 
 <body>
+    <!-- nav -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand text-brand" href="index.html">
+                <img src="assets/img/logo.png" alt="" width="50px" />
+                Jardinería del Éden</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#">Inicio</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Galería</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#about1">Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#products">Prodcutos</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="#">Admin</a></li> -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                        Admin
+                    </button>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- modal -->
+    <div class="modal" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Jardinería del Éden</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <form class="form-signin">
+                        <div class="img-login mx-auto">
+                            <img class="mb-4 img-fluid " src="assets/img/logo.png" alt="" width="120">
+                        </div>
+                        <h1 class="h3 mb-3 font-weight-normal">Por favor, introduce las credenciales</h1>
+                        <!-- user -->
+                        <label for="inputEmail" class="sr-only">Usuario</label>
+                        <input type="text" id="inputEmail" class="form-control m-3 mx-auto" placeholder="Usuario"
+                            required="" autofocus="">
+                        <!-- password -->
+                        <label for="inputPassword" class="sr-only">Contraseña</label>
+                        <input type="password" id="inputPassword" class="form-control m-3 mx-auto"
+                            placeholder="Contraseña" required="">
+
+                        <!-- <button class="btn btn-lg btn-primary btn-block" type="submit" onClick="return validar()">
+                  Ir </button> -->
+                        <!-- doesnt working -->
+                        <input type="button" class="btn btn-lg btn-primary btn-block" value="Ir"
+                            onClick="return validar()">
+
+
+                        <p class="mt-5 mb-3 text-muted">© 2019-2022</p>
+                    </form>
+
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!-- main -->
     <div class="container my-5">
-        <a href="add.php" class="btn btn-lg btn-outline-primary">Agregar</a>
+        <a href="add.php" class="my-5 btn btn-lg btn-outline-primary">Agregar</a>
 
 
         <div class="table-responsive my-5">
@@ -30,7 +122,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                   <?php                  
+                    <?php                  
                    $sql = "SELECT * FROM macetas";
                    $result = mysqli_query($connection, $sql);
                    if (mysqli_num_rows($result) > 0) {
@@ -42,7 +134,7 @@
                         $observaciones = $row['bd_observaciones'];
                         $tamano = $row['bd_tamano'];
                         $color = $row['bd_color'];
-                 ?> 
+                 ?>
                     <tr>
                         <td><?php echo $id; ?></td>
                         <td><?php echo $categoria; ?></td>
@@ -50,19 +142,18 @@
                         <td><?php echo $cantidad; ?></td>
                         <td><?php echo $observaciones ?></td>
                         <td><?php echo  $tamano ?></td>
-                        <td><?php echo  $color ?></td>                      
-                       
+                        <td><?php echo  $color ?></td>
+
                         <td>
-                        <a href="eliminar.php?deleteid=<?php echo $id ?>"
-                         class="btn btn-outline-danger"
-                         onclick="return confirmation()">
-                         Eliminar</a>
-                         
-                        <a href="actualizar.php?updateid=<?php echo $id ?>"
-                         class="btn  btn-outline-dark">Actualizar</a>
-                       
+                            <a href="eliminar.php?deleteid=<?php echo $id ?>" class="btn btn-outline-danger"
+                                onclick="return confirmation()">
+                                Eliminar</a>
+
+                            <a href="actualizar.php?updateid=<?php echo $id ?>"
+                                class="btn  btn-outline-dark">Actualizar</a>
+
                         </td>
-                    </tr>                    
+                    </tr>
                     <?php
                 }
                 
@@ -78,19 +169,82 @@
 
 
 
+    <footer class="footer_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 footer-col">
+                    <div class="footer_contact">
+                        <h4>Contactanos</h4>
+                        <div class="contact_link_box">
+                            <a href="https://goo.gl/maps/AFLsBTwAWwEGr1636" target="_blank">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                <span> Ubicación </span>
+                            </a>
+                            <a href="https://wa.me/527861015488/?text=Hola, ¿Qué tal?" target="_blank">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <span> Llama +52 786 1015488 </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <span> jardinera.eden@gmail.com </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <a href="" class="footer-logo"> Jardinería del Éden </a>
+                        <p>Vive tu vida, vive tus plantas</p>
+                        <div class="footer_social">
+                            <a href="">
+                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <h4>Horario</h4>
+                    <p>Todos los días</p>
+                    <p>10.00 Am - 10.00 Pm</p>
+                </div>
+            </div>
+            <div class="footer-info">
+                <p>
+                    &copy; <span id="displayYear"></span> Todos los derecho reservados -
+                    <i>Jardinería del Éden</i>
+                    <!-- <a href="https://html.design/">Free Html Templates</a><br><br> -->
+                    <!--   &copy; <span id="displayYear"></span> Distributed By
+              <a href="https://themewagon.com/" target="_blank">ThemeWagon</a> -->
+                </p>
+            </div>
+        </div>
+    </footer>
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
+    <script src="https://kit.fontawesome.com/0c7e5557d4.js" crossorigin="anonymous"></script>
 
-<script >
-  function confirmation(){
-    if (confirm("¿Realmente desea eliminar el registro?")) {
-      return true;
+    <script>
+    function confirmation() {
+        if (confirm("¿Realmente desea eliminar el registro?")) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
-
-</script>
+    </script>
 </body>
+
 </html>
